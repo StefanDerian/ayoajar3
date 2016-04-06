6c87700e6e769a675faae670e030fdd662cd1b1f
StefanDerian <Stefan_sippo@yahoo.com>
2016-04-06 23:10:38 +0700

    progress
    yeah

--- wp-content/plugins/stefan-search-teacher/stefan-search-teacher.php
+++ wp-content/plugins/stefan-search-teacher/stefan-search-teacher.php
@@ -6,8 +6,23 @@ Author: Stefan
 Version: 1.0.0
 */
 
-	
 
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+class search_teacher{
+
+	public function show_teacher_form(){
+
+
+
+
+		
+	}
+
+
+
+}
 
 
 
